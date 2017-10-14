6b9bb35f0b719642f085f09571f65a89a9e70f98
Ganesh Patro <30416891+ganeshpatro321@users.noreply.github.com>
2017-10-14 18:27:35 +0530

    Add files via upload
    ejs sequence

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 
 
-var express = require('express')
+var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
-var app = express()
+var app = express();
 var port = 8080;
+
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
 var router = require('./app/routes');
 app.use('/',router);
 
-app.set('view engine', 'ejs');
-app.use(expressLayouts);
+
 
 
 app.use(express.static(__dirname + '/public'));
